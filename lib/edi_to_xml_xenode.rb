b7f6b34824cc992113e8eb9bcbff498e4dd27b94
Vance Pan <vancep@nodally.com>
2013-11-28 12:30:25 -0800

    Added readme content to the code for easier reference

--- lib/edi_to_xml_xenode.rb
+++ lib/edi_to_xml_xenode.rb
@@ -2,6 +2,29 @@
 # Licensed under the Open Software License version 3.0
 # http://opensource.org/licenses/OSL-3.0
 
+# version 0.1.0
+#
+# EDI-to-XML Xenode parses EDI documents that it receives in its input message data and converts the data 
+# into XML format, using the EDI field heads as tag names. Each incoming message with data should include an 
+# EDI document serialized into a string. Set 'indent' to true in the Configuration File if you prefer to 
+# include proper indentation of the fields in the output XML file. Set 'include_headers' to true if you want 
+# to include header tags for the output XML. 
+#
+# Configuration File Options:###
+#   loop_delay: defines number of seconds the Xenode waits before running the Xenode process. Expects a float. 
+#   enabled: determines if the Xenode process is allowed to run. Expects true/false.
+#   debug: enables extra debug messages in the log file. Expects true/false.
+#   indent: specifies whether the output XML tags should include indentation. Expects true/false.
+#   include_headers: specifies whether the output XML should include header fields. Expects true/false.
+#
+# Example Configuration File:###
+#   enabled: false
+#   loop_delay: 30
+#   debug: false
+#   indent: true
+#   include_headers: true
+#
+
 #xeno-meta:EdiToXmlXenode
 
 # require the edi_parse lib
@@ -26,7 +49,7 @@ class EdiToXmlXenode
         edi.parse()
         xml_text = edi.to_xml(@indent, @include_headers)
         # forward the message to children
-        # msg.msg_id = msg.newid
+        msg.msg_id = msg.newid
         msg.data = xml_text
         write_to_children(msg)
       end
